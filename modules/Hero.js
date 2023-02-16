73672396552614f756dc785c7faaa9d5f7e2899c
Odis Barnett <69765129+othewebdev@users.noreply.github.com>
2023-02-16 14:15:11 -0600

    [Attempt] Fix mobile hero issues + remove react-device-detect

--- modules/Hero.js
+++ modules/Hero.js
@@ -1,5 +1,4 @@
 import styles from "@/styles/Hero.module.css";
-import { isMobile, isBrowser, MobileView } from "react-device-detect";
 import { VideoTag } from "react-video-tag";
 import videoBg from "../videos/webvideoOpt.webm";
 import ActiveLink from "./ActiveLink";
@@ -7,17 +6,15 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <div className={isMobile ? styles.mobileHero : styles.hero}>
-      {isBrowser && (
-        <VideoTag
-          className={styles.backVideo}
-          src={videoBg + "#t=0.001"}
-          autoPlay
-          loop
-          muted
-          playsInline
-        />
-      )}
+    <div className={styles.hero}>
+      <VideoTag
+        className={styles.backVideo}
+        src={videoBg + "#t=0.001"}
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
       <div className={styles.innerContainer}>
         <div className={styles.innerTextContainer}>
           <h1 data-text="HQ Records">HQ Records</h1>
