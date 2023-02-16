d4fc0bf8ed68cd30e6bb8b1ee7c0de45e65da82b
Odis Barnett <69765129+othewebdev@users.noreply.github.com>
2023-02-16 16:06:47 -0600

    [Success] fix grid for artist page

--- modules/ArtistInfo.js
+++ modules/ArtistInfo.js
@@ -1,21 +1,15 @@
-import Image from "next/image";
 import React from "react";
 import styles from "@/styles/ArtistInfo.module.css";
 
 const ArtistInfo = ({ artist }) => {
   return (
-    <div className={styles.artistInfo}>
-      <div className={styles.innerInfo}>
-        <div className={styles.artistImage}>
-          <Image
-            className={styles.innerImage}
-            src={artist.image || ""}
-            alt={artist.name + " " + "Biography"}
-            loading="eager"
-          />
+    <div className={styles.artistInfoContainer}>
+      <div className={styles.artistInfoGrid}>
+        <div id="col1">
+          <p>{artist.name}</p>
         </div>
-        <div className={styles.artistBio}>
-          <p className={styles.innerBio}>{artist.biography}</p>
+        <div id="col2">
+          <div>{artist.biography}</div>
         </div>
       </div>
     </div>
