ab514dd4b6e77e4c47ec44a6c2c5e86876615338
Odis Barnett <69765129+othewebdev@users.noreply.github.com>
2023-02-16 13:33:21 -0600

    [Attempt] Introducing react-video-tag to fix mobile video issue

--- modules/Hero.js
+++ modules/Hero.js
@@ -1,71 +1,21 @@
-import React, { useRef, useState, useEffect } from "react";
 import styles from "@/styles/Hero.module.css";
+import Image from "next/image";
+import { VideoTag } from "react-video-tag";
 import videoBg from "../videos/webvideoOpt.webm";
 import ActiveLink from "./ActiveLink";
 import Button from "./Button";
-import Image from "next/image";
-
-const isSafari = () => {
-  const ua = navigator.userAgent.toLowerCase();
-  return ua.indexOf("safari") > -1 && ua.indexOf("chrome") < 0;
-};
 
 const Hero = () => {
-  const videoParentRef = useRef();
-
-  const [shouldUseImage, setShouldUseImage] = useState(false);
-  useEffect(() => {
-    // check if user agent is safari and we have the ref to the container <div />
-    if (isSafari() && videoParentRef.current) {
-      // obtain reference to the video element
-      const player = videoParentRef.current.children[0];
-
-      // if the reference to video player has been obtained
-      if (player) {
-        // set the video attributes using javascript as per the
-        // webkit Policy
-        player.controls = false;
-        player.playsinline = true;
-        player.muted = true;
-        player.setAttribute("muted", ""); // leave no stones unturned :)
-        player.autoplay = true;
-
-        // Let's wait for an event loop tick and be async.
-        setTimeout(() => {
-          // player.play() might return a promise but it's not guaranteed crossbrowser.
-          const promise = player.play();
-          // let's play safe to ensure that if we do have a promise
-          if (promise.then) {
-            promise
-              .then(() => {})
-              .catch(() => {
-                // if promise fails, hide the video and fallback to <img> tag
-                videoParentRef.current.style.display = "none";
-                setShouldUseImage(true);
-              });
-          }
-        }, 0);
-      }
-    }
-  }, []);
-
   return (
     <div className={styles.hero}>
-      <video
-        ref={videoParentRef}
-        preload="metadata"
+      <VideoTag
         className={styles.backVideo}
+        src={videoBg}
         autoPlay
         loop
         muted
         playsInline
-      >
-        {shouldUseImage ? (
-          <Image src="" alt="" />
-        ) : (
-          <source src={videoBg + "#t=0.001"} type="video/mp4" />
-        )}
-      </video>
+      />
       <div className={styles.innerContainer}>
         <div className={styles.innerTextContainer}>
           <h1 data-text="HQ Records">HQ Records</h1>
