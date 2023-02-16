16801832f861f099267babcf9c13d4c07f2b3b72
Odis Barnett <69765129+othewebdev@users.noreply.github.com>
2023-02-16 14:12:01 -0600

    [Attempt] Conditional styling for mobile with ext package

--- modules/Hero.js
+++ modules/Hero.js
@@ -1,5 +1,5 @@
 import styles from "@/styles/Hero.module.css";
-import Image from "next/image";
+import { isMobile, isBrowser, MobileView } from "react-device-detect";
 import { VideoTag } from "react-video-tag";
 import videoBg from "../videos/webvideoOpt.webm";
 import ActiveLink from "./ActiveLink";
@@ -7,15 +7,17 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <VideoTag
-        className={styles.backVideo}
-        src={videoBg + "#t=0.001"}
-        autoPlay
-        loop
-        muted
-        playsInline
-      />
+    <div className={isMobile ? styles.mobileHero : styles.hero}>
+      {isBrowser && (
+        <VideoTag
+          className={styles.backVideo}
+          src={videoBg + "#t=0.001"}
+          autoPlay
+          loop
+          muted
+          playsInline
+        />
+      )}
       <div className={styles.innerContainer}>
         <div className={styles.innerTextContainer}>
           <h1 data-text="HQ Records">HQ Records</h1>
