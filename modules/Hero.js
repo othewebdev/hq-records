3aadb7d327360de4b803a09f071deab775456f71
Odis Barnett <69765129+othewebdev@users.noreply.github.com>
2023-02-16 13:06:48 -0600

    [Attempt] Fix mobile safari video

--- modules/Hero.js
+++ modules/Hero.js
@@ -1,27 +1,20 @@
 import styles from "@/styles/Hero.module.css";
-import { useEffect, useRef } from "react";
 import videoBg from "../videos/webvideoOpt.webm";
 import ActiveLink from "./ActiveLink";
 import Button from "./Button";
 
 const Hero = () => {
-  const videoRef = useRef(undefined);
-  useEffect(() => {
-    videoRef.current.defaultMuted = true;
-  });
-
   return (
     <div className={styles.hero}>
       <video
-        ref={videoRef}
+        preload="metadata"
         className={styles.backVideo}
-        controls
         autoPlay
         loop
         muted
         playsInline
       >
-        <source src={videoBg} type="video/mp4" />
+        <source src={videoBg + "#t=0.001"} type="video/mp4" />
       </video>
       <div className={styles.innerContainer}>
         <div className={styles.innerTextContainer}>
