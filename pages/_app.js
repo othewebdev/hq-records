4a7dff9e32fcc7c2ab6648582208f098fb149164
othewebdev <kingo.iv@gmail.com>
2023-02-16 20:02:02 -0600

    [Attempt] Move consent var to _document

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,11 @@ import { DynamicFooter, DynamicHeader } from "@/modules/dynamic";
 import "@/styles/globals.css";
 import { Inter } from "@next/font/google";
 import Head from "next/head";
-
-import { useState } from "react";
 import Layout from "@/modules/layout";
-import { getCookie } from "cookies-next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  const [transitionEffect, setTransitionEffect] = useState(1);
-  const consent = getCookie("localConsent");
-
   return (
     <>
       <Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
+import { getCookie } from "cookies-next";
 
 export default function Document() {
+  const consent = getCookie("localConsent");
   return (
     <Html lang="en">
       <Head>
