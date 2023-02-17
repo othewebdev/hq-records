322e3e7ce378d67a1fc758bb737fe63d2ce89d0e
othewebdev <kingo.iv@gmail.com>
2023-02-17 01:22:06 -0600

    [Success] Artist page styling

--- modules/ArtistInfo.js
+++ modules/ArtistInfo.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "@/styles/ArtistInfo.module.css";
+import Image from "next/image";
 
 const ArtistInfo = ({ artist }) => {
   return (
     <div className={styles.artistInfoContainer}>
       <div className={styles.artistInfoGrid}>
-        <div id="imgColumn"></div>
-        <div id="bioColumn">
+        <div id={styles.imgColumn}>
+          <Image src={artist.image} alt={artist.name} />
+        </div>
+        <div id={styles.bioColumn}>
           <p id={styles.artistName}>{artist.name}</p>
           <span>
             <p>{artist.location}</p>
