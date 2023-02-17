4a1ac333cef8eb99c7602f7ae8a6a9911d45fb3d
othewebdev <kingo.iv@gmail.com>
2023-02-16 22:15:54 -0600

    [Success] Google Tag + Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,43 +3,40 @@ import Script from "next/script";
 import { getCookie } from "cookies-next";
 
 export default function Document() {
+  let GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
   const consent = getCookie("localConsent");
   return (
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Catamaran:wght@400;500;600&display=swap"
-          rel="stylesheet"
-        />
+
         <link
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"
         ></link>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-        <Script
-          id="gtag_mid"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-           window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-FRMYPLFSLF');`,
-          }}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
         />
-        <Script
-          id="gtag"
-          strategy="afterInteractive"
+        <script
+          async
           dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
-            
+            gtag('js', new Date());
+            gtag('config', '${GTM_ID}');
+        `,
+          }}
+        />
+        <script
+          async
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
             gtag('consent', 'default', {
               'ad_storage': 'denied',
               'analytics_storage': 'denied'
@@ -48,16 +45,11 @@ export default function Document() {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WBVXLQX');`,
+            })(window,document,'script','dataLayer','${GTM_ID}');`,
           }}
         />
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-FRMYPLFSLF"
-        />
         {consent === true && (
-          <Script
-            id="consupd"
+          <script
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
@@ -69,6 +61,10 @@ export default function Document() {
             }}
           />
         )}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
