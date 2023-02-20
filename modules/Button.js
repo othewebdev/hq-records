b6f5c8d7cc19055f3f55160b12328a59855894a0
Odis Barnett <69765129+othewebdev@users.noreply.github.com>
2023-02-20 16:59:42 -0600

    [Success] Contact form + recaptcha

--- modules/Button.js
+++ modules/Button.js
@@ -1,9 +1,14 @@
 import React from "react";
 import styles from "@/styles/Button.module.css";
 
-const Button = ({ text, onClick }) => {
+const Button = ({ text, onClick, disabled, type, secondary }) => {
   return (
-    <button className={styles.primaryButton} onClick={() => onClick}>
+    <button
+      className={secondary ? styles.secondaryButton : styles.primaryButton}
+      onClick={() => onClick}
+      disabled={disabled}
+      type={type}
+    >
       {text}
     </button>
   );
--- modules/ContactForm.js
+++ modules/ContactForm.js
@@ -1,66 +1,115 @@
-import React from 'react'
-import { Formik } from 'formik'
+import React, { useState } from "react";
+import { Formik } from "formik";
+import ReCAPTCHA from "react-google-recaptcha";
+import styles from "@/styles/ContactForm.module.css";
+import Button from "./Button";
 
 const ContactForm = () => {
+  const [showRecaptcha, setShowRecaptcha] = useState(false);
   return (
     // Copy and pasted form below from Formik
     // Change to Contact Form, Configure and Style :)
-    <div>
+    <div className={styles.outer}>
+      <div className={styles.inner}>
         <Formik
-       initialValues={{ email: '', password: '' }}
-       validate={values => {
-         const errors = {};
-         if (!values.email) {
-           errors.email = 'Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address';
-         }
-         return errors;
-       }}
-       onSubmit={(values, { setSubmitting }) => {
-         setTimeout(() => {
-           alert(JSON.stringify(values, null, 2));
-           setSubmitting(false);
-         }, 400);
-       }}
-     >
-       {({
-         values,
-         errors,
-         touched,
-         handleChange,
-         handleBlur,
-         handleSubmit,
-         isSubmitting,
-         /* and other goodies */
-       }) => (
-         <form onSubmit={handleSubmit}>
-           <input
-             type="email"
-             name="email"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.email}
-           />
-           {errors.email && touched.email && errors.email}
-           <input
-             type="password"
-             name="password"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.password}
-           />
-           {errors.password && touched.password && errors.password}
-           <button type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-         </form>
-       )}
-     </Formik>
+          initialValues={{ email: "", password: "", message: "", subject: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = "A valid email is required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+            if (!values.subject) {
+              errors.subject = "A valid subject is required";
+            }
+            if (!values.message) {
+              errors.message = "A valid message is required";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit} className={styles.contactForm}>
+              <label>Email</label>
+              <input
+                type="email"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              <p style={{ color: "red" }}>
+                {errors.email && touched.email && errors.email}
+              </p>
+              <label>Subject</label>
+              <input
+                type="text"
+                name="subject"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.subject}
+              />
+              <p style={{ color: "red" }}>
+                {errors.subject && touched.subject && errors.subject}
+              </p>
+              <label>Message</label>
+              <textarea
+                type="text"
+                name="message"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.message}
+              />
+              <p style={{ color: "red" }}>
+                {errors.message && touched.message && errors.message}
+              </p>
+              <div>
+                <p>Please verify ReCAPTCHA below:</p>
+                <ReCAPTCHA
+                  sitekey="6LcBC50kAAAAAE1lXSnjk9oSFyxaRxxwtiEe-LeW"
+                  render="explicit"
+                  theme="light"
+                  verifyCallback={(response) => {
+                    setFieldValue("recaptcha", response);
+                  }}
+                  onloadCallback={() => {
+                    setShowRecaptcha(true);
+                  }}
+                />
+                {errors.recaptcha && touched.recaptcha && (
+                  <p>{errors.recaptcha}</p>
+                )}
+              </div>
+              <Button
+                text="submit"
+                disabled={isSubmitting}
+                type="submit"
+                secondary
+              />
+            </form>
+          )}
+        </Formik>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
