c96e8a105a541ae9b0826d2a49fcc01bf7bed167
Odis Barnett <69765129+othewebdev@users.noreply.github.com>
2023-02-28 16:43:38 -0600

    [Success] About section

--- modules/AboutInfo.js
+++ modules/AboutInfo.js
@@ -1,12 +1,24 @@
-import React, { useCallback } from "react";
 import styles from "@/styles/AboutInfo.module.css";
 
 const AboutInfo = () => {
   return (
     <div className={styles.outer}>
       <div className={styles.inner}>
-        <div className={styles.row}></div>
-        <div className={styles.row}>about</div>
+        <div className={styles.column}>
+          <h1>Image goes here...</h1>
+        </div>
+        <div className={styles.column}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+            corporis autem exercitationem harum consequatur, ab suscipit
+            adipisci tempore quis nihil provident recusandae porro voluptatibus
+            dicta repellat, tempora, perferendis consequuntur expedita? Illo
+            temporibus, quis laboriosam cupiditate architecto, laborum
+            asperiores iste deserunt debitis officia fuga sit alias! Tempora
+            minima suscipit ipsum optio sapiente in error vitae, magni accusamus
+            non cupiditate ea neque.
+          </p>
+        </div>
       </div>
     </div>
   );
